aff66329e30cf563a5cdc02376480171500565d7
shaik amir <shaikamir2003@gmail.com>
2024-03-24 16:17:35 +0530

    demo9

--- AddLoopsGIF_streamlit2.py
+++ AddLoopsGIF_streamlit2.py
@@ -64,7 +64,7 @@ def main():
     else:
         # image = Image.open('input.gif')
         #st.video("https://github.com/shaikamirgh/Custom-Boot-Animation/blob/main/input.gif")
-        st.video(input.gif, format='video/mp4')
+        st.video("input.gif", format='video/mp4')
         #st.markdown("![Default GIF for you to try! Drag and Drop!](https://github.com/shaikamirgh/Custom-Boot-Animation/blob/main/input.gif)")
         #st.markdown("![Default GIF for you to try! Drag and Drop!](https://media.giphy.com/media/og52So0BUmZVe/giphy.gif)")
 
